f778e7a9da129d9a96e08dab7c9eecbc6bd7f41e
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-28 16:24:35 -0300

    Delete src directory

--- src/main/java/com/tuyo/gerenciamentotransacao/ContaBancariaRepo.java
+++
@@ -1,7 +0,0 @@
-package com.tuyo.gerenciamentotransacao;
-
-import com.tuyo.gerenciamentotransacao.entities.ContaBancaria;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContaBancariaRepo extends CrudRepository<ContaBancaria, Integer> {
-}
