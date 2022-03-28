2f7988a86034ddce7bf79d7361b246c777e8d58d
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-28 16:25:48 -0300

    Create contabanco.sql

---
+++ contabanco.sql
@@ -0,0 +1,10 @@
+-- Criando tabela com as colunas de outra forma:
+create table contabanco (accno int,lastname varchar(25),firstname varchar(25),bal int)
+
+insert into contabanco values(1,'silva','joao',5000)
+
+insert into contabanco values(2,'anitta','maria',4000)
+
+select * from contabanco
+
+drop table contabanco
