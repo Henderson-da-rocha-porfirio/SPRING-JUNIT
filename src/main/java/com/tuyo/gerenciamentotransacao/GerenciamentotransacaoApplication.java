77f1008bb97ee4a181f29acec6fb7611cdf23be7
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-28 15:54:01 -0300

    # commit 1

---
+++ src/main/java/com/tuyo/gerenciamentotransacao/GerenciamentotransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.tuyo.gerenciamentotransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentotransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentotransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuyo/gerenciamentotransacao/GerenciamentotransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyo.gerenciamentotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentotransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
