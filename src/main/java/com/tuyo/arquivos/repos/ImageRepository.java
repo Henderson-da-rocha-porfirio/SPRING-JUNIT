9f3ee5cb181ccd1da36ccdfff3ecb3d28ee27ab3
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-29 09:26:12 -0300

    Delete src directory

--- src/main/java/com/tuyo/arquivos/repos/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tuyo.arquivos.repos;
-
-import com.tuyo.arquivos.entities.Image;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ImageRepository extends CrudRepository<Image, Long> {
-
-}
